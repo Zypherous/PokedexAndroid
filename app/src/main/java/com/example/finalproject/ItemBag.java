ec75773d91b4daee4908c6a658934d2cb1c79af5
dchowdhury-mdsol <daiyanch22@gmail.com>
2022-06-24 00:56:58 -0400

    created template for view

--- app/src/main/java/com/example/finalproject/ItemBag.java
+++ app/src/main/java/com/example/finalproject/ItemBag.java
@@ -14,6 +14,8 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.NetworkResponse;
@@ -27,40 +29,43 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.util.Locale;
 
 public class ItemBag extends AppCompatActivity {
-    SurfaceView surfaceView;
-    SurfaceHolder surfaceHolder;
-    TextView textView;
-    Button button;
-    Bitmap background;
-    Canvas backgroundCanvas;
+    Button drawCards, searchButtonId;
+    TextView playerName;
+    EditText searchBar;
+    ImageView playerAvatar, pokeLogo, pokeViews;
+
+    JsonObjectRequest[] jsonImageQueries = new JsonObjectRequest[6];
+
+//    SurfaceView surfaceView;
+//    SurfaceHolder surfaceHolder;
+//    TextView textView;
+
+//    View pokeView;
+//    Bitmap background;
+//    Canvas backgroundCanvas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.item_bag);
 
-        surfaceView = findViewById(R.id.surfaceView);
-        surfaceHolder = surfaceView.getHolder();
-        textView = findViewById(R.id.textView);
-        button.findViewById(R.id.button);
+//        surfaceView = findViewById(R.id.surfaceView);
+//        surfaceHolder = surfaceView.getHolder();
+//        textView = findViewById(R.id.textView);
+//        button.findViewById(R.id.button);
+
+//        pokeView = findViewById(R.id.PokeViewStandardId);
         String pokemonName = "pikachu";
         String url = String.format("https://pokeapi.co/api/v2/pokemon/%s", pokemonName.toLowerCase(Locale.ROOT));
         RequestQueue queue = Volley.newRequestQueue(this);
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, (JSONObject) null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                textView.setText(response.toString());
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText(error.toString());
-            }
-        });
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url,
+                (JSONObject) null, response -> textView.setText(response.toString()),
+                error -> textView.setText(error.toString()));
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
